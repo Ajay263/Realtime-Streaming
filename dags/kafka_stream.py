554aeca0a397b1d0a973d07f05dd189890854060
Alexio Junior Aaron <97569992+Ajay263@users.noreply.github.com>
2024-01-08 09:35:12 +0000

    Updated the api endpoint

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -5,7 +5,7 @@ from airflow.operators.python import PythonOperator
 
 default_args = {
     'owner': 'Ajay',
-    'start_date': datetime(2024, 01, 8, 11, 21)
+    'start_date': datetime(2024, 1, 8, 11, 21)
 }
 
 def get_data():
@@ -43,23 +43,24 @@ def stream_data():
     import logging
 
     producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
-    curr_time = time.time()
+    producer.send('users_created', json.dumps(res).encode('utf-8'))
+    # curr_time = time.time()
 
-    while True:
-        if time.time() > curr_time + 60: #1 minute
-            break
-        try:
-            res = get_data()
-            res = format_data(res)
+    # while True:
+    #     if time.time() > curr_time + 60: #1 minute
+    #         break
+    #     try:
+    #         res = get_data()
+    #         res = format_data(res)
 
-            producer.send('users_created', json.dumps(res).encode('utf-8'))
-        except Exception as e:
-            logging.error(f'An error occured: {e}')
-            continue
+    #         producer.send('users_created', json.dumps(res).encode('utf-8'))
+    #     except Exception as e:
+    #         logging.error(f'An error occured: {e}')
+    #         continue
 
 with DAG('user_automation',
          default_args=default_args,
-         schedule_interval='@daily',
+         schedule='@daily',
          catchup=False) as dag:
 
     streaming_task = PythonOperator(
