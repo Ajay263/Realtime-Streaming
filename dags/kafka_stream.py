22c778a45e6c9cb6a219f551bc9df8bb7d0e28c6
Alexio Junior Aaron <97569992+Ajay263@users.noreply.github.com>
2024-01-08 11:47:15 +0000

    Added entrypoint script

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -4,8 +4,8 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 default_args = {
-    'owner': 'Ajay',
-    'start_date': datetime(2024, 1, 8, 11, 21)
+    'owner': 'airscholar',
+    'start_date': datetime(2023, 9, 3, 10, 00)
 }
 
 def get_data():
@@ -20,7 +20,7 @@ def get_data():
 def format_data(res):
     data = {}
     location = res['location']
-    data['id'] = uuid.uuid4()
+   
     data['first_name'] = res['name']['first']
     data['last_name'] = res['name']['last']
     data['gender'] = res['gender']
@@ -38,13 +38,15 @@ def format_data(res):
 def stream_data():
     import json
     from kafka import KafkaProducer
-    import time
-    import logging
+    # import time
+    # import logging
     res = get_data()
     res = format_data(res)
 
     producer = KafkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000)
+    
     producer.send('users_created', json.dumps(res).encode('utf-8'))
+    
     # curr_time = time.time()
 
     # while True:
@@ -59,12 +61,14 @@ def stream_data():
     #         logging.error(f'An error occured: {e}')
     #         continue
 
-with DAG('user_automation',
-         default_args=default_args,
-         schedule='@daily',
-         catchup=False) as dag:
+# with DAG('user_automation',
+#          default_args=default_args,
+#          schedule_interval='@daily',
+#          catchup=False) as dag:
 
-    streaming_task = PythonOperator(
-        task_id='stream_data_from_api',
-        python_callable=stream_data
-    )
+#     streaming_task = PythonOperator(
+#         task_id='stream_data_from_api',
+#         python_callable=stream_data
+#     )
+
+stream_data()
